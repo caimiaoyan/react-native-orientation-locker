5cd62d837a8e50e93efcdc6fc1d7e31f4b20f8f6
蔡妙燕 <caimiaoyan@szclou.com>
2025-05-21 15:14:11 +0800

    修改代码

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-import Orientation from './android_js_src/Orientation';
+import Orientation from './mian';
 
-export * from './android_js_src/hooks';
+export * from './main';
 
 export default Orientation;
 
-export const OrientationType = {
-  'PORTRAIT': 'PORTRAIT',
-  'PORTRAIT-UPSIDEDOWN': 'PORTRAIT-UPSIDEDOWN',
-  'LANDSCAPE-LEFT': 'LANDSCAPE-LEFT',
-  'LANDSCAPE-RIGHT': 'LANDSCAPE-RIGHT',
-  'FACE-UP': 'FACE-UP',
-  'FACE-DOWN': 'FACE-DOWN',
-  'UNKNOWN': 'UNKNOWN',
-};
